ff078295966224b0bf719d3d2aa4cead917d95e1
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-21 12:01:42 +0530

    Learn about CoreHaptics

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -6,118 +6,54 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ContentView: View {
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
+    @State private var engine: CHHapticEngine?
     
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        // create one intense, sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
     
     var body: some View {
-        
-        let dragGesture = DragGesture()
-            .onChanged { (value) in
-                self.offset = value.translation
-            }
-            .onEnded { _ in
-                withAnimation {
-                    self.isDragging = false
-                    self.offset = CGSize.zero
-                }
-            }
-        
-        let pressGesture = LongPressGesture()
-            .onEnded { _ in
-                withAnimation {
-                    self.isDragging = true
-                }
-            }
-        
-        let combine = pressGesture.sequenced(before: dragGesture)
-        
-        
-        return Circle()
-            .fill(Color.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combine)
+        Text("Hello World")
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
     }
 }
-    
-
-//struct ContentView: View {
-//        @State private var currentAmount: CGFloat = 0
-//        @State private var finalAmount: CGFloat = 1
-//
-//    @State private var currentAmount: Angle = .degrees(0)
-//    @State private var finalAmount: Angle = .degrees(0)
-//
-//
-//    var body: some View {
-//                Text("Hello, world!")
-//                    .padding()
-//                    .onTapGesture(count: 2, perform: {
-//                        print("Double tap")
-//                    })
-//                Text("Hello, World!")
-//                    .onLongPressGesture(minimumDuration: 2, pressing: { inProgress in
-//                        print("In progress: \(inProgress)!")
-//                    }) {
-//                        print("Long pressed!")
-//                    }
-//
-//
-//                Text("Hello, World!")
-//                    .scaleEffect(finalAmount + currentAmount)
-//                    .gesture(
-//                        MagnificationGesture()
-//                            .onChanged { (amount) in
-//                                self.currentAmount = amount - 1
-//                            }
-//                            .onEnded { (amount) in
-//                                self.finalAmount += self.currentAmount
-//                                self.currentAmount = 0
-//                            }
-//                    )
-//
-//
-//
-//                Text("Hello, World!")
-//                    .rotationEffect(currentAmount + finalAmount)
-//                    .gesture(
-//                        RotationGesture()
-//                            .onChanged { angle in
-//                                self.currentAmount = angle
-//                            }
-//                            .onEnded { angle in
-//                                self.finalAmount += self.currentAmount
-//                                self.currentAmount = .degrees(0)
-//                            }
-//                    )
-//
-//
-//
-//        VStack {
-//            Text("Hello World")
-//                .onTapGesture {
-//                    print("Child Tapped!!")
-//                }
-//        }
-//                .highPriorityGesture(
-//                    TapGesture()
-//                        .onEnded({ _ in
-//                            print("Parent Tapped!!")
-//                        })
-//                )
-//        .simultaneousGesture(
-//            TapGesture()
-//                .onEnded({ _ in
-//                    print("Parent Tapped!!")
-//                })
-//        )
-//
-//    }
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
