fc3b1328eeabadd96efa79330ffce1d678171ede
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-22 02:25:09 +0530

    supporting-specific-accessibility-needs-with-swiftui

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,20 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityReduceMotion) var reduceMotion
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
+    
+    @State private var scale: CGFloat = 1
+    
+    func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+        if UIAccessibility.isReduceMotionEnabled {
+            return try body()
+        } else {
+            return try withAnimation(animation, body)
+        }
+    }
+    
     var body: some View {
         VStack {
-            Text("Hello world")
-                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification), perform: { _ in
-                    print("Moving to the background!")
-                })
+            Text("Hello, World!")
+                .scaleEffect(scale)
+                .onTapGesture {
+                    withOptionalAnimation {
+                        self.scale *= 1.5
+                    }
+                }
             
-            Text("Hello World Two")
-                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification), perform: { _ in
-                    print("Moving back to the foreground!")
-                })
+            Text("Hello, World!")
+                .padding()
+                .background(reduceTransparency ? Color.black : Color.black.opacity(0.5))
+                .foregroundColor(Color.white)
+                .clipShape(Capsule())
+            
+            HStack {
+                if differentiateWithoutColor {
+                    Image(systemName: "checkmark.circle")
+                }
+                
+                Text("Success")
+            }
+            .padding()
+            .background(differentiateWithoutColor ? Color.black : Color.green)
+            .foregroundColor(Color.white)
+            .clipShape(Capsule())
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
