48dc7574ab08e965f43ed845db128351a27273d5
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-22 02:09:41 +0530

    Learn notificaion when app is going to background and foreground

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,22 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-//    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
-    
-    @State private var counter = 0
-    
     var body: some View {
-        Text("Hello world")
-            .onReceive(timer, perform: { time in
-                if self.counter == 5 {
-                    self.timer.upstream.connect().cancel()
-                } else {
-                    print("The time is now \(time)")
-                }
-                
-                self.counter += 1
-            })
+        VStack {
+            Text("Hello world")
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification), perform: { _ in
+                    print("Moving to the background!")
+                })
+            
+            Text("Hello World Two")
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification), perform: { _ in
+                    print("Moving back to the foreground!")
+                })
+        }
     }
 }
 
