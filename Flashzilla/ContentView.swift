685487502b51c8bca68176a511fbfba8b32b1546
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-21 12:55:55 +0530

    learn about allowHitTesting and contentShape

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -6,52 +6,38 @@
 //
 
 import SwiftUI
-import CoreHaptics
 
 struct ContentView: View {
-    @State private var engine: CHHapticEngine?
-    
-    func simpleSuccess() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.success)
-    }
-    
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        do {
-            self.engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was an error creating the engine: \(error.localizedDescription)")
-        }
-    }
-    
-    func complexSuccess() {
-        // make sure that the device supports haptics
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        var events = [CHHapticEvent]()
-        
-        // create one intense, sharp tap
-        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
-        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
-        events.append(event)
-        
-        // convert those events into a pattern and play it immediately
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription).")
-        }
-    }
-    
     var body: some View {
-        Text("Hello World")
-            .onAppear(perform: prepareHaptics)
-            .onTapGesture(perform: complexSuccess)
+        VStack {
+            ZStack {
+                Rectangle()
+                    .fill(Color.blue)
+                    .frame(width: 300, height: 300)
+                    .onTapGesture {
+                        print("Rectangle tapped!")
+                    }
+                
+                Circle()
+                    .fill(Color.red)
+                    .frame(width: 300, height: 300)
+                    //                .contentShape(Rectangle())
+                    .onTapGesture {
+                        print("Circle tapped!")
+                    }
+                    .allowsHitTesting(false)
+            }
+            
+            VStack {
+                Text("Hello")
+                Spacer().frame(height: 100)
+                Text("World")
+            }
+            .contentShape(Rectangle())
+            .onTapGesture {
+                print("VStack tapped!")
+            }
+        }
     }
 }
 
